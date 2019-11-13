da7aa57ffa2fa0375c635ee5979b05e50fd110b2
WhiteOwl314 <parksj914@naver.com>
2019-11-13 17:40:24 +0900

    complete views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Question, Coice
 
 
@@ -13,13 +14,22 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question': question, 'choice': choice, })
+    return render(request, "polls/detail.html", {'question': question, })
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.coice_set.get(pk=request.POST['choice'])
+    except (KeyError, Coice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select choice.", },)
+    else:
+        selected_choice.cotes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
